4261374adc4c73cd713bb22ab563b54f08e4427e
JuanSaavedra <elsharpos@gmail.com>
2015-11-04 15:53:06 +1100

    added order date time

--- Parser/Parser/Order.cs
+++ Parser/Parser/Order.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Parser
 {
   public class Order
   {
     public string SourceSystem { get; set; }
+
+    public DateTime OrderDateTime { get; set; }
 
     public List<OrderItem> Items { get; set; }
 
