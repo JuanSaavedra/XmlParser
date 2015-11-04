6ba39a2d0b2b1e43608779729add3abc179de529
Juan Saavedra <elsharpos@gmail.com>
2015-11-04 13:11:40 +1100

    added changes

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
 namespace Parser
 {
-  class Program
+  public class OrderParser
+  {
+    
+  }
+
+  public class Program
   {
     static void Main(string[] args)
     {
+
     }
   }
 }
