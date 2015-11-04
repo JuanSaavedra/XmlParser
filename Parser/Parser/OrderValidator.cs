24d7c44ed9fe93363d10c4846ba96568d52d9582
JuanSaavedra <elsharpos@gmail.com>
2015-11-04 16:49:58 +1100

    added Order validator

---
+++ Parser/Parser/OrderValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser
+{
+  public class OrderValidator
+  {
+    public bool ValidateOrder(Order order)
+    {
+
+      return false;
+    }
+  }
+}
