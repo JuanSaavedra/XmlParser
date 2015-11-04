af37f29cf2ff0b0d04916f4aceaaab273f43ad8b
JuanSaavedra <elsharpos@gmail.com>
2015-11-04 13:59:59 +1100

    Moved the Order parser into its own file

---
+++ Parser/Parser/OrderParser.cs
@@ -0,0 +1,13 @@
+namespace Parser
+{
+  public class OrderParser
+  {
+    public Order ParseOrder()
+    {
+      var order = new Order();
+
+
+      return order;
+    }
+  }
+}
--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -5,17 +5,6 @@ using System.Text;
 
 namespace Parser
 {
-  public class OrderParser
-  {
-    public Order ParseOrder()
-    {
-      var order = new Order();
-
-
-      return order;
-    }
-  }
-
   public class Program
   {
     static void Main(string[] args)
